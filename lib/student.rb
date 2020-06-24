1835f353b1382a31f78add6d34f6e71a09d9738a
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 23:04:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,6 @@ attr_accessor :name, :grade, :id
       VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs") [0][0]
   end
 
