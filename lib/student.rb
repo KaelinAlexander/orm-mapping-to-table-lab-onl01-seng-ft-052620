c26776586d0fe99d583936f2d5126eaf2c2330a3
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 22:43:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,10 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
+@@all = []
+
+
 def initalize(name, grade)
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+
 
 end
